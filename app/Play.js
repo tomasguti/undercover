119dd1ea160a057278f7b1d52b3ee36adbdf7c80
Tomás Gutiérrez <tomasguti@gmail.com>
2023-05-12 20:07:35 -0300

    More words and button debounce

--- app/Play.js
+++ app/Play.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './components/button';
 import { useStorage, SCREEN } from './storage/StorageProvider'
 import styles from './page.module.css'
 
 export default function Play() {
   const { state: { word, turn, currentPlayerName, finished, hidden }, dispatch } = useStorage();
+  const [disableNext, setDisableNext] = useState(false);
+
+  const debounceButton = () => {
+    setDisableNext(true);
+    setTimeout(() => {
+      setDisableNext(false)}, 1000);
+  };
 
   const nextLabel = hidden ? 'Mostrar' : 'Siguiente';
   const roundEnded = turn === SCREEN.RESULT && finished;
@@ -16,11 +23,14 @@ export default function Play() {
         <h1>{word}</h1>
       </div>
       <h2>{currentPlayerName || ''}</h2>
-      <Button onClick={() => {
-        dispatch({
-          type: 'next_turn',
-          value: '',
-        });
+      <Button disabled={disableNext} onClick={() => {
+        if (!disableNext) {
+          debounceButton();
+          dispatch({
+            type: 'next_turn',
+            value: '',
+          });
+        }
       }}>
         { buttonLabel }
       </Button>
--- app/components/button.js
+++ app/components/button.js
@@ -2,7 +2,7 @@ import styles from './button.module.css'
 
 export default function Button({ children, ...props }) {
   return (
-    <div { ...props } className={styles.button}>
+    <div { ...props } className={`${styles.button} ${props.disabled && styles.disabled}`}>
       { children }
     </div>
   )
